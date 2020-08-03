0c6c71f74ed5cb61dd1f9aca354ee5e69d88ec38
Kate Skorija <kateskorija@gmail.com>
2020-08-03 10:14:00 -0700

    set up files and run initial migration
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public Item()
+    {
+      this.Categories = new HashSet<CategoryItem>();
+    }
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+    public virtual ICollection<CategoryItem> Categories { get; }
+  }
+}
